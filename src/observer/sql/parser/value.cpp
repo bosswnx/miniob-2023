5e0b7461919bb09a40dd6ce4e61d2245178bdcc8
Soulter <905617992@qq.com>
2023-11-02 17:08:07 +0800

    fix: null值的比较

--- src/observer/sql/parser/value.cpp
+++ src/observer/sql/parser/value.cpp
@@ -201,14 +201,14 @@ std::string Value::to_string() const
 int Value::compare(const Value &other) const
 {
   if (this->is_null_) {
-    // null 是最大的
+    // null 是最小的
     if (other.is_null_) {
       return 0;
     } else {
-      return 1;
+      return -1;
     }
   } else if (other.is_null_) {
-    return -1;
+    return 1;
   }
   if (this->attr_type_ == other.attr_type_) {
     switch (this->attr_type_) {
